d8a55d51ce1c61e052b125f9665082ca7bce18cb
nttt2003 <thuthuy05032003@gmail.com>
2024-05-28 13:15:37 +0700

    Love

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Anh yêu à!',
+    introDesc: `Ngắm hoàng hôn mà quên trời đã tối
+    Ngắm anh cười mà bối rối cả con tim :v :v`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là yêu em <33',
+    btnNo: 'Không, Cô bé à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em nha <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Hehe, sến vch, nói chung anh là của tôi Dương ạ. Nhắn tin cho mình để nói yêu mình nha <3 :v',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/thu.thuy.nguyen.2k3/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
